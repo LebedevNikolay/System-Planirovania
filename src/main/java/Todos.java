c869fb8a3e2d2256f09398afe80e4a6781471bf0
nikolay <nikolay-kmc@mail.ru>
2024-08-27 23:35:00 +0300

    newTodosTest

--- src/main/java/Todos.java
+++ src/main/java/Todos.java
@@ -1,107 +1,52 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+public class Todos {
 
-public class TodosTest {
+    private Task[] tasks = new Task[0]; // <- тут будут все задачи
 
-    @Test
-    public void shouldAddThreeTasksOfDifferentType() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        Todos todos = new Todos();
-
-        todos.add(simpleTask);
-        todos.add(epic);
-        todos.add(meeting);
-
-        Task[] expected = {simpleTask, epic, meeting};
-        Task[] actual = todos.findAll();
-        Assertions.assertArrayEquals(expected, actual);
+    /**
+     * Вспомогательный метод для имитации добавления элемента в массив
+     *
+     * @param current Массив, в который мы хотим добавить элемент
+     * @param task    Элемент, который мы хотим добавить
+     * @return Возвращает новый массив, который выглядит как тот, что мы передали,
+     * но с добавлением нового элемента в конец
+     */
+    private Task[] addToArray(Task[] current, Task task) {
+        Task[] tmp = new Task[current.length + 1];
+        for (int i = 0; i < current.length; i++) {
+            tmp[i] = current[i];
+        }
+        tmp[tmp.length - 1] = task;
+        return tmp;
     }
 
-    @Test
-    public void shouldResponceSeveral() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = {"Приложение НетоБанка", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        Todos todos = new Todos();
-
-        todos.add(simpleTask);
-        todos.add(epic);
-        todos.add(meeting);
-
-        Task[] expected = {epic, meeting};
-        Task[] actual = todos.search("Приложение НетоБанка");
-        Assertions.assertArrayEquals(expected, actual);
+    /**
+     * Метод добавления задачи в список дел
+     *
+     * @param task Добавляемая задача
+     */
+    public void add(Task task) { // <- вот здесь в параметре может лежать объект и вида SimpleTask, и вида Epic, и вида Meeting
+        tasks = addToArray(tasks, task);
     }
 
-    @Test
-    public void shouldResponceZero() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = {"Приложение НетоБанка", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        Todos todos = new Todos();
-
-        todos.add(simpleTask);
-        todos.add(epic);
-        todos.add(meeting);
-
-        Task[] expected = {};
-        Task[] actual = todos.search("Суббота");
-        Assertions.assertArrayEquals(expected, actual);
+    public Task[] findAll() {
+        return tasks;
     }
 
-    @Test
-    public void shouldResponceOne() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = {"Приложение НетоБанка", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        Todos todos = new Todos();
-
-        todos.add(simpleTask);
-        todos.add(epic);
-        todos.add(meeting);
-
-        Task[] expected = {simpleTask};
-        Task[] actual = todos.search("Позвонить родителям");
-        Assertions.assertArrayEquals(expected, actual);
+    /**
+     * Метод поиска задач, которые подходят под поисковый запрос
+     *
+     * @param query Поисковый запрос
+     * @return Массив из подошедших задач
+     */
+    public Task[] search(String query) {
+        Task[] result = new Task[0]; // массив для ответа
+        for (Task task : tasks) { // перебираем все задачи
+            if (task.matches(query)) { // если задача подходит под запрос
+                result = addToArray(result, task); // добавляем её в массив ответа
+            }
+        }
+        return result;
     }
 }
